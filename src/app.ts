b0219fa4c53c919d7e5eef74be8003e86907b1d1
Sakhawat0pu <s19hossain98@gmail.com>
2023-11-22 17:12:58 -0500

    src folder has been added, connection to the database has been made, anda config file for the environment variable has been created

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+
+const app: Application = express();
+
+// Parser
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req: Request, res: Response) => {
+	res.status(200).send({
+		success: true,
+		message: "A server for mongoose assignment",
+	});
+});
+
+export default app;
